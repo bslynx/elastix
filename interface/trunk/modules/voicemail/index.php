e66dd9e8e54286fd1f7ee8109ae5ef4e03d96e8f
adonisfigueroa <adonisfigueroa@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2008-04-01 20:59:06 +0000

    Module voicemail changed to make the report faster and teh admin can view all extensions

--- interface/trunk/modules/voicemail/index.php
+++ interface/trunk/modules/voicemail/index.php
@@ -43,7 +43,7 @@ function _moduleContent(&$smarty, $module_name)
     $base_dir=dirname($_SERVER['SCRIPT_FILENAME']);
     $templates_dir=(isset($arrConfig['templates_dir']))?$arrConfig['templates_dir']:'themes';
     $local_templates_dir="$base_dir/modules/$module_name/".$templates_dir.'/'.$arrConf['theme'];
-    
+
     //segun el usuario que esta logoneado consulto si tiene asignada extension para buscar los voicemails
     $pDB = new paloDB("sqlite3:////var/www/db/acl.db");
 
@@ -59,9 +59,13 @@ function _moduleContent(&$smarty, $module_name)
     $arrVoiceData = array();
     $inicio= $fin = $total = 0;
     $extension = $pACL->getUserExtension($_SESSION['elastix_user']);
+    $esAdministrador = $pACL->isUserAdministratorGroup($_SESSION['elastix_user']);
+    if($esAdministrador)
+        $extension = "[[:digit:]]+";
+
     $smarty->assign("menu","voicemail");
     $smarty->assign("Filter",$arrLang['Filter']);
-   //formulario para el filtro
+    //formulario para el filtro
     $arrFormElements = array("date_start"  => array("LABEL"                  => $arrLang["Start Date"],
                                                         "REQUIRED"               => "yes",
                                                         "INPUT_TYPE"             => "DATE",
@@ -75,7 +79,7 @@ function _moduleContent(&$smarty, $module_name)
                                                         "VALIDATION_TYPE"        => "ereg",
                                                         "VALIDATION_EXTRA_PARAM" => "^[[:digit:]]{1,2}[[:space:]]+[[:alnum:]]{3}[[:space:]]+[[:digit:]]{4}$"),
                                  );
-    
+
     $oFilterForm = new paloForm($smarty, $arrFormElements);
         // Por omision las fechas toman el sgte. valor (la fecha de hoy)
     $date_start = date("Y-m-d") . " 00:00:00"; 
@@ -100,7 +104,6 @@ function _moduleContent(&$smarty, $module_name)
                 $smarty->assign("mb_message", $strErrorMsg);
             }
             $htmlFilter = $oFilterForm->fetchForm("$local_templates_dir/filter.tpl", "", $_POST);
-    
     } else if (isset($_GET['date_start']) AND isset($_GET['date_end'])) {
         $date_start = translateDate($_GET['date_start']) . " 00:00:00";
         $date_end   = translateDate($_GET['date_end']) . " 23:59:59";
@@ -124,100 +127,101 @@ function _moduleContent(&$smarty, $module_name)
         $htmlFilter = $oFilterForm->fetchForm("$local_templates_dir/filter.tpl", "", $_POST);
     }
 
+    $end = 0;
     //si tiene extension consulto sino, muestro un mensaje de que no tiene asociada extension
     $archivos=array();
     if (!is_null($extension)){
         $path = "/var/spool/asterisk/voicemail/default";
         $folder = "INBOX";
-    
-        $voicemailPath = "$path/$extension/$folder";
-        if (file_exists($voicemailPath)) {
-            if ($handle = opendir($voicemailPath)) {
-                $bExito=true;
-                while (false !== ($file = readdir($handle))) {
-                //no tomar en cuenta . y ..
-                    if ($file!="." && $file!=".." )
-                        $archivos[]=$file;
-                }
-                closedir($handle);
-            }
-        } else {
-            // No vale la ruta
-        }
-        rsort($archivos);
-        foreach($archivos as $archivo) {
-            $arrTmp    = array();
-            //buscar los archivos de texto (txt) que son los que contienen los datos de las llamadas 
-            if (ereg("(.+)\.[txt|TXT]",$archivo,$regs)){
-                //leer la info del archivo
-                $pConfig = new paloConfig($voicemailPath, $archivo, "=", "[[:space:]]*=[[:space:]]*");
-                $arrVoiceMailDes=array();
-                $arrVoiceMailDes = $pConfig->leer_configuracion(false);
-                //verifico que tenga datos
-                if (is_array($arrVoiceMailDes) && count($arrVoiceMailDes)>0 && isset($arrVoiceMailDes['origtime']['valor'])){
-                    //uso las fechas del filtro
-                    //si la fecha de llamada esta dentro del rango, la muestro
-                    $fecha = date("Y-m-d",$arrVoiceMailDes['origtime']['valor']);
-                    $hora = date("H:i:s",$arrVoiceMailDes['origtime']['valor']);
-                    
-                    if (strtotime("$fecha $hora")<=strtotime($date_end) && strtotime("$fecha $hora")>=strtotime($date_start)){
-                        $arrTmp[0] = "<input type='checkbox' name='".utf8_encode("voc-".$archivo)."' />";
-                        $arrTmp[1] = $fecha;
-                        $arrTmp[2] = $hora;
-                        $arrTmp[3] = $arrVoiceMailDes['callerid']['valor'];
-                        $arrTmp[4] = $arrVoiceMailDes['origmailbox']['valor'];
-                        $arrTmp[5] = $arrVoiceMailDes['duration']['valor'].' sec.';
-                        $pathRecordFile="$voicemailPath/".$regs[1].'.wav';
-                        $recordingLink = "<a href='#' onClick=\"javascript:popUp('includes/popup.php?action=display_record&record_file=" . base64_encode($pathRecordFile) ."',350,100); return false;\">Listen</a>&nbsp;";
-                        $recordingLink .= "<a href='includes/audio.php?recording=".base64_encode($pathRecordFile)."'>Download</a>";
-                        $arrTmp[6] = $recordingLink;
-                        $arrData[] = $arrTmp;
+
+        if($esAdministrador)
+        {
+            if ($handle = opendir($path)) {
+                while (false !== ($dir = readdir($handle))) {
+                    if ($dir != "." && $dir != ".." && ereg($extension, $dir, $regs) && is_dir($path."/".$dir)) {
+                        $directorios[] = $dir;
                     }
                 }
             }
-        }
+        }else $directorios[] = $extension;
 
+        $arrData = array();
+        foreach($directorios as $directorio)
+        {
+            $voicemailPath = "$path/$directorio/$folder";
+            if (file_exists($voicemailPath)) {
+                if ($handle = opendir($voicemailPath)) {
+                    $bExito=true;
+                    while (false !== ($file = readdir($handle))) {
+                        //no tomar en cuenta . y ..
+                        //buscar los archivos de texto (txt) que son los que contienen los datos de las llamadas
+                        if ($file!="." && $file!=".." && ereg("(.+)\.[txt|TXT]",$file,$regs))
+                        {
+                            //leer la info del archivo
+                            $pConfig = new paloConfig($voicemailPath, $file, "=", "[[:space:]]*=[[:space:]]*");
+                            $arrVoiceMailDes=array();
+                            $arrVoiceMailDes = $pConfig->leer_configuracion(false);
 
-        $total=count($arrData);
-    // LISTADO
-        
-        $limit = 15;
-        $offset = 0;
-    
-        // Si se quiere avanzar a la sgte. pagina
-        if($_GET['nav']=="end") {
-    
-            // Mejorar el sgte. bloque.
-            if(($total%$limit)==0) {
-                $offset = $total - $limit;
+                            //verifico que tenga datos
+                            if (is_array($arrVoiceMailDes) && count($arrVoiceMailDes)>0 && isset($arrVoiceMailDes['origtime']['valor'])){
+                                //uso las fechas del filtro
+                                //si la fecha de llamada esta dentro del rango, la muestro
+                                $fecha = date("Y-m-d",$arrVoiceMailDes['origtime']['valor']);
+                                $hora = date("H:i:s",$arrVoiceMailDes['origtime']['valor']);
+
+                                if (strtotime("$fecha $hora")<=strtotime($date_end) && strtotime("$fecha $hora")>=strtotime($date_start)){
+                                    $arrTmp[0] = "<input type='checkbox' name='".utf8_encode("voc-".$file)."' />";
+                                    $arrTmp[1] = $fecha;
+                                    $arrTmp[2] = $hora;
+                                    $arrTmp[3] = $arrVoiceMailDes['callerid']['valor'];
+                                    $arrTmp[4] = $arrVoiceMailDes['origmailbox']['valor'];
+                                    $arrTmp[5] = $arrVoiceMailDes['duration']['valor'].' sec.';
+                                    $pathRecordFile="$voicemailPath/".$regs[1].'.wav';
+                                    $recordingLink = "<a href='#' onClick=\"javascript:popUp('includes/popup.php?action=display_record&record_file=" . base64_encode($pathRecordFile) ."',350,100); return false;\">Listen</a>&nbsp;";
+                                    $recordingLink .= "<a href='includes/audio.php?recording=".base64_encode($pathRecordFile)."'>Download</a>";
+                                    $arrTmp[6] = $recordingLink;
+                                    $arrData[] = $arrTmp;
+                                }
+                            }
+                        }
+                    }
+                    closedir($handle);
+                }
             } else {
-                $offset = $total - $total%$limit;
+                // No vale la ruta
             }
         }
-    
-        // Si se quiere avanzar a la sgte. pagina
-        if($_GET['nav']=="next") {
-            $offset = $_GET['start'] + $limit - 1;
+        /*
+        function sort_voicemails_hora_desc($a, $b) { return ($a[2] == $b[2]) ? 0 : (($a[2] < $b[2]) ? 1 : -1); }
+        function sort_voicemails_fecha_desc($a, $b) { return ($a[1] == $b[1]) ? 0 : (($a[1] < $b[1]) ? 1 : -1); }
+        usort($arrData, 'sort_voicemails_hora_desc');
+        usort($arrData, 'sort_voicemails_fecha_desc');
+        */
+        foreach ($arrData as $llave => $fila) {
+            $fechas[$llave]  = $fila[1];
+            $horas[$llave] = $fila[2];
         }
-    
-        // Si se quiere retroceder
-        if($_GET['nav']=="previous") {
-            $offset = $_GET['start'] - $limit - 1;
-        }
-    
+        array_multisort($fechas,SORT_DESC,$horas,SORT_DESC,$arrData);
+
+        //Paginacion
+        $limit  = 15;
+        $total  = count($arrData);
+
+        $oGrid  = new paloSantoGrid($smarty);
+        $offset = $oGrid->getOffSet($limit,$total,(isset($_GET['nav']))?$_GET['nav']:NULL,(isset($_GET['start']))?$_GET['start']:NULL);
+
+        $end    = ($offset+$limit)<=$total ? $offset+$limit : $total;
+
         // Construyo el URL base
-        if(is_array($arrFilterExtraVars) and count($arrFilterExtraVars)>0) {
-            $url = construirURL($arrFilterExtraVars, array("nav", "start")); 
+        if(isset($arrFilterExtraVars) && is_array($arrFilterExtraVars) and count($arrFilterExtraVars)>0) {
+            $url = construirURL($arrFilterExtraVars, array("nav", "start"));
         } else {
             $url = construirURL(array(), array("nav", "start")); 
         }
         $smarty->assign("url", $url);
-    
-        $inicio = ($total==0) ? 0 : $offset + 1;
-        $fin = ($offset+$limit)<=$total ? $offset+$limit : $total;
-        $leng=$fin-$inicio;
-        //muestro los registros correspondientes al offset
-        $arrVoiceData=array_slice($arrData,$inicio-1,$leng+1);
+        //Fin Paginacion
+
+        $arrVoiceData=array_slice($arrData, $offset, $limit);
     } //fin if (!is_null(extension))
     else {
         $smarty->assign("mb_message", "<b>".$arrLang["You don't have extension number associated with user"]."</b>");
@@ -225,8 +229,8 @@ function _moduleContent(&$smarty, $module_name)
     $arrGrid = array("title"    => $arrLang["Voicemail List"],
                      "icon"     => "images/record.png",
                      "width"    => "99%",
-                     "start"    => $inicio,
-                     "end"      => $fin,
+                     "start"    => ($total==0) ? 0 : $offset + 1,
+                     "end"      => $end,
                      "total"    => $total,
                      "columns"  => array(0 => array("name"      => "<input type='submit' onClick=\"return confirmSubmit('{$arrLang["Are you sure you wish to delete voicemails?"]}');\" name='submit_eliminar' value='{$arrLang["Delete"]}' class='button' />",
                                                     "property1" => ""),
@@ -258,7 +262,7 @@ function borrarVoicemails($extension)
     $path = "/var/spool/asterisk/voicemail/default";
     $folder = "INBOX";
     $voicemailPath = "$path/$extension/$folder";
-    
+
     if(is_array($_POST) && count($_POST) > 0){
         foreach($_POST as $name => $on){
             if(substr($name,0,4)=='voc-'){
