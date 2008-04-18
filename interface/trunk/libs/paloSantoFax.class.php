d215f79e489f30ecd2561f9b4c2161c7ebd88224
bpalosanto <bpalosanto@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2008-04-18 19:55:07 +0000

    Fixed bug, faxes not show.

--- interface/trunk/libs/paloSantoFax.class.php
+++ interface/trunk/libs/paloSantoFax.class.php
@@ -142,7 +142,7 @@ class paloFax {
         if ($db = sqlite3_open($this->rutaDB)) {
             $query  = "SELECT id, name, extension, secret, clid_name, clid_number, dev_id, date_creation, email FROM fax";
             $result = sqlite3_query($db, $query);
-            if(is_array($result))
+            if(isset($result))
             {
                 while ($row = sqlite3_fetch_array($result)) {
                     $arrReturn[]=$row;
