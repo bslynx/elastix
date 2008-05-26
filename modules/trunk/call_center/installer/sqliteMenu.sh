1cc7379b80811b5cac3151bf89702a3bd41252de
bpalosanto <bpalosanto@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2008-05-26 20:40:09 +0000

    Better implementation in insert menus in sqlite database.

--- modules/trunk/call_center/installer/sqliteMenu.sh
+++ modules/trunk/call_center/installer/sqliteMenu.sh
@@ -2,12 +2,21 @@
 #Script para mejorar la presentacion de los menus. A tres niveles.
 #Esto solo es un parche para corregir lo que actualmete el paloSantoInstaller y paloSantoModuloXML
 #no esta implementa del soporte para tres niveles de menus.
-sqlite3 /var/www/db/acl.db   "insert into acl_resource(name,description) values ('forms','Forms'); insert into acl_group_permission(id_action,id_group,id_resource) values (1,1,(select last_insert_rowid()));"
+existeMenuForm=`sqlite3 /var/www/db/menu.db "select count(*) from menu where id='forms'"`
+existeMenuReportIngoingCall=`sqlite3 /var/www/db/menu.db "select count(*) from menu where id='reports_ingoing_call'"`
+
+if [ $existeMenuForm -eq 0 ]; then
+    sqlite3 /var/www/db/menu.db  "insert into menu values ('forms','call_center','','Forms','module');"
+    sqlite3 /var/www/db/acl.db   "insert into acl_resource(name,description) values ('forms','Forms'); insert into acl_group_permission(id_action,id_group,id_resource) values (1,1,(select last_insert_rowid()));"
+fi
+
+if [ $existeMenuReportIngoingCall -eq 0 ]; then
+sqlite3 /var/www/db/menu.db  "insert into menu values ('reports_ingoing_call','call_center','','Reports','module');"
 sqlite3 /var/www/db/acl.db   "insert into acl_resource(name,description) values ('reports_ingoing_call','Reports'); insert into acl_group_permission(id_action,id_group,id_resource) values (1,1,(select last_insert_rowid()));"
-sqlite3 /var/www/db/menu.db  "insert into menu values ('forms','call_center','','Forms','module');"
+fi
+
 sqlite3 /var/www/db/menu.db  "update menu set idparent='forms' where id='form_designer';"
 sqlite3 /var/www/db/menu.db  "update menu set idparent='forms' where id='form_list';"
-sqlite3 /var/www/db/menu.db  "insert into menu values ('reports_ingoing_call','call_center','','Reports','module');"
 sqlite3 /var/www/db/menu.db  "update menu set idparent='reports_ingoing_call' where id='reports_break';"
 sqlite3 /var/www/db/menu.db  "update menu set idparent='reports_ingoing_call' where id='calls_detail';"
 sqlite3 /var/www/db/menu.db  "update menu set idparent='reports_ingoing_call' where id='calls_per_hour';" 
