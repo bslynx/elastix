e2717a41cbf28e136738c19447a1e709db6f9212
adonisfigueroa <adonisfigueroa@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2008-06-09 20:53:46 +0000

    Files to generate graphs were purge

--- interface/trunk/images/pie.php
+++ interface/trunk/images/pie.php
@@ -1,35 +1,66 @@
-<?php 
-/* vim: set expandtab tabstop=4 softtabstop=4 shiftwidth=4:
-  Codificación: UTF-8
-  +----------------------------------------------------------------------+
-  | Elastix version 0.5                                                  |
-  | http://www.elastix.org                                               |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 2006 Palosanto Solutions S. A.                         |
-  +----------------------------------------------------------------------+
-  | Cdla. Nueva Kennedy Calle E 222 y 9na. Este                          |
-  | Telfs. 2283-268, 2294-440, 2284-356                                  |
-  | Guayaquil - Ecuador                                                  |
-  | http://www.palosanto.com                                             |
-  +----------------------------------------------------------------------+
-  | The contents of this file are subject to the General Public License  |
-  | (GPL) Version 2 (the "License"); you may not use this file except in |
-  | compliance with the License. You may obtain a copy of the License at |
-  | http://www.opensource.org/licenses/gpl-license.php                   |
-  |                                                                      |
-  | Software distributed under the License is distributed on an "AS IS"  |
-  | basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See  |
-  | the License for the specific language governing rights and           |
-  | limitations under the License.                                       |
-  +----------------------------------------------------------------------+
-  | The Original Code is: Elastix Open Source.                           |
-  | The Initial Developer of the Original Code is PaloSanto Solutions    |
-  +----------------------------------------------------------------------+
-  $Id: pie.php,v 1.1.1.1 2007/07/06 21:31:56 gcarrillo Exp $ */
+<?php
+require_once("../libs/jpgraph/jpgraph.php");
+require_once("../libs/jpgraph/jpgraph_pie.php");
+require_once("../libs/jpgraph/jpgraph_pie3d.php");
+dl('sqlite3.so');
+//cargar el idioma
+include_once("../libs/misc.lib.php");
+load_language("../");
 
-require_once("../libs/paloSantoGraph.class.php"); 
-$img = new paloGraph(140, 140, "fafafa"); 
-$espacio_restante = 100 - $_GET["du"]; 
-$img->crearPie3D(array($_GET['du'], $espacio_restante), array("FF6600", "3333CC", "CC3333", "72394a", "aa3424")); 
-$img->genSalida(); 
+if(ereg("^([[:digit:]]{1,3})%", trim($_GET['du']), $arrReg)) {
+    $usoDisco = $arrReg[1];
+} else {
+    $usoDisco = $_GET['du'];
+}
+$freeDisco = 100 - $usoDisco;
+
+// Some data
+$data = array($usoDisco, $freeDisco);
+
+// Create the Pie Graph.
+$graph = new PieGraph(630, 170,"auto");
+//$graph->SetShadow();
+$graph->SetMarginColor('#fafafa');
+$graph->SetFrame(true,'#999999');
+
+$graph->legend->SetFillColor("#fafafa");
+//$graph->legend->Pos(0.012, 0.5, "right","center");
+$graph->legend->SetColor("#444444", "#999999");
+$graph->legend->SetShadow('gray@0.6',4);
+
+//$graph->title->SetColor("#444444");
+
+// Set A title for the plot
+$graph->title->Set(utf8_decode($arrLang["Disk usage"]));
+//$graph->title->SetFont(FF_VERDANA,FS_BOLD,18);
+$graph->title->SetColor("#444444");
+$graph->legend->Pos(0.1,0.2);
+
+// Create 3D pie plot
+$p1 = new PiePlot3d($data);
+//$p1->SetTheme("water");
+$p1->SetSliceColors(array("#3333cc", "#9999cc", "#CC3333", "#72394a", "#aa3424")); 
+$p1->SetCenter(0.4);
+$p1->SetSize(80);
+
+// Adjust projection angle
+$p1->SetAngle(45);
+
+// Adjsut angle for first slice
+$p1->SetStartAngle(45);
+
+// Display the slice values
+//$p1->value->SetFont(FF_ARIAL,FS_BOLD,11);
+//$p1->value->SetColor("navy");
+$p1->value->SetColor("black");
+
+// Add colored edges to the 3D pie
+// NOTE: You can't have exploded slices with edges!
+$p1->SetEdge("black");
+
+$p1->SetLegends(array(utf8_decode($arrLang["Used space"]),utf8_decode($arrLang["Free space"])));
+
+$graph->Add($p1);
+$graph->Stroke();
+
 ?>
