13b50ce410abe0f039beb1d86664c5d3abe4c68b
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2008-07-17 22:09:17 +0000

    Cuotes simple to  doubles. This is more standar.

--- interface/trunk/libs/misc.lib.php
+++ interface/trunk/libs/misc.lib.php
@@ -626,13 +626,13 @@ function checkbox($id_name, $checked='off', $disable='off')
     $check = $disab = "";
 
     if(!($checked=='off'))
-        $check = "checked='checked'";
+        $check = "checked=\"checked\"";
     if(!($disable=='off'))
-        $disab = "disabled='disabled'";
+        $disab = "disabled=\"disabled\"";
 
-    $checkbox  = "<input type='checkbox' name='chkold{$id_name}' $check $disab onclick='javascript:{$id_name}check();' /> 
-                  <input type='hidden'   name='{$id_name}' id='{$id_name}'   value='{$checked}' />
-                  <script type='text/javascript'>
+    $checkbox  = "<input type=\"checkbox\" name=\"chkold{$id_name}\" $check $disab onclick=\"javascript:{$id_name}check();\" /> 
+                  <input type=\"hidden\"   name=\"{$id_name}\" id=\"{$id_name}\"   value=\"{$checked}\" />
+                  <script type=\"text/javascript\">
                     function {$id_name}check(){
                         var node = document.getElementById('$id_name');
                         if(node.value == 'on')
