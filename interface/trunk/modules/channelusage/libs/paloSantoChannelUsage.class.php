b312a26b0643c13d0cd14b139d6b3c52cd445b10
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2009-03-24 20:11:21 +0000

    Fix hardcoded path to document root

--- interface/trunk/modules/channelusage/libs/paloSantoChannelUsage.class.php
+++ interface/trunk/modules/channelusage/libs/paloSantoChannelUsage.class.php
@@ -1,9 +1,10 @@
 <?php
 
-require_once "/var/www/html/libs/misc.lib.php";
+global $arrConf;
+require_once $arrConf['basePath']."/libs/misc.lib.php";
 
-require_once "/var/www/html/libs/paloSantoDB.class.php";
-require_once "/var/www/html/libs/paloSantoSampler.class.php";
+require_once $arrConf['basePath']."/libs/paloSantoDB.class.php";
+require_once $arrConf['basePath']."/libs/paloSantoSampler.class.php";
 
 class paloSantoChannelUsage
 {
