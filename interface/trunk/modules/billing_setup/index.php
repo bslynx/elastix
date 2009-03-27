7f583f9ff38b3dee34d511fa34b7b89c0757633c
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2009-03-27 17:29:44 +0000

    Separation billing setup from framwork , modify files and add some languages [#147]

--- interface/trunk/modules/billing_setup/index.php
+++ interface/trunk/modules/billing_setup/index.php
@@ -38,11 +38,24 @@ function _moduleContent(&$smarty, $module_name)
     
     //include module files
     include_once "modules/$module_name/configs/default.conf.php";
+    
+    $lang=get_language();
+    $base_dir=dirname($_SERVER['SCRIPT_FILENAME']);
+    $lang_file="modules/$module_name/lang/$lang.lang";
+    if (file_exists("$base_dir/$lang_file")) include_once "$lang_file";
+    else include_once "modules/$module_name/lang/en.lang";
+
+    //global variables
     global $arrConf;
+    global $arrConfModule;
     global $arrLang;
+    global $arrLangModule;
+    $arrConf = array_merge($arrConf,$arrConfModule);
+    $arrLang = array_merge($arrLang,$arrLangModule);
+
     //folder path for custom templates
     $base_dir=dirname($_SERVER['SCRIPT_FILENAME']);
-    $templates_dir=(isset($arrConfig['templates_dir']))?$arrConfig['templates_dir']:'themes';
+    $templates_dir=(isset($arrConf['templates_dir']))?$arrConf['templates_dir']:'themes';
     $local_templates_dir="$base_dir/modules/$module_name/".$templates_dir.'/'.$arrConf['theme'];
     
 
@@ -55,7 +68,7 @@ function _moduleContent(&$smarty, $module_name)
                $arrConfig['AMPDBHOST']['valor'] . "/asterisk";
     $pDB     = new paloDB($dsn);
     $pDBSetting = new paloDB($arrConf['elastix_dsn']['settings']);
-    $pDBTrunk = new paloDB("sqlite3:///$arrConf[elastix_dbdir]/trunk.db");
+    $pDBTrunk = new paloDB($arrConfModule['dsn_conn_database_1']);
     $arrForm  = array("default_rate"       => array("LABEL"                   => $arrLang["Default Rate"],
                                                     "REQUIRED"               => "yes",
                                                     "INPUT_TYPE"             => "TEXT",
