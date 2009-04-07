24d618982505731654e3993350ea9d8220c898cb
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2009-04-07 17:54:55 +0000

    Update module becouse it was detecting zaptel "ZTDUMMY/1" configuration while it had been finding dahdi configuration [#147]

--- interface/trunk/modules/hardware_detection/index.php
+++ interface/trunk/modules/hardware_detection/index.php
@@ -39,22 +39,23 @@ function _moduleContent(&$smarty, $module_name)
     include_once "modules/$module_name/configs/default.conf.php";
     
     $lang=get_language();
-    $script_dir=dirname($_SERVER['SCRIPT_FILENAME']);
+    $base_dir=dirname($_SERVER['SCRIPT_FILENAME']);
     $lang_file="modules/$module_name/lang/$lang.lang";
-    if (file_exists("$script_dir/$lang_file"))
-        include_once($lang_file);
-    else
-        include_once("modules/$module_name/lang/en.lang");
+    if (file_exists("$base_dir/$lang_file")) include_once "$lang_file";
+    else include_once "modules/$module_name/lang/en.lang";
 
+    //global variables
     global $arrConf;
+    global $arrConfModule;
     global $arrLang;
     global $arrLangModule;
+    $arrConf = array_merge($arrConf,$arrConfModule);
     $arrLang = array_merge($arrLang,$arrLangModule);
 //     print_r($_POST);
     require_once "modules/$module_name/libs/PaloSantoHardwareDetection.class.php";
     //folder path for custom templates
     $base_dir=dirname($_SERVER['SCRIPT_FILENAME']);
-    $templates_dir=(isset($arrConfig['templates_dir']))?$arrConfig['templates_dir']:'themes';
+    $templates_dir=(isset($arrConf['templates_dir']))?$arrConf['templates_dir']:'themes';
     $local_templates_dir="$base_dir/modules/$module_name/".$templates_dir.'/'.$arrConf['theme'];
     
     $xajax = new xajax();
@@ -82,7 +83,7 @@ function listPorts($smarty, $module_name, $local_templates_dir) {
     $smarty->assign("detectandoHardware",$arrLang['Hardware Detecting']);
     $smarty->assign("CARD",$arrLang['Card']);
     $smarty->assign("CARD_MISDN",$arrLang['Misdn Card']);
-    $smarty->assign("CARD_NO_MOSTRAR",'ZTDUMMY/1');
+    $smarty->assign("CARD_NO_MOSTRAR",'DAHDI');
     $smarty->assign("PORT_NOT_FOUND",$arrLang['Ports not Founds']);
     //$smarty->assign("NO_PUERTO",$arrLang['No. Port']);
     $smarty->assign("NO_PUERTO",$arrLang["Port"]." ");
