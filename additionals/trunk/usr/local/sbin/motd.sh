1d8bdb56dc6355d26ca31c7cbe64310bf816e7b7
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2009-05-15 20:48:24 +0000

    New script for add in the elastix addicionals

---
+++ additionals/trunk/usr/local/sbin/motd.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+IFCONFIG=`which ifconfig 2>/dev/null||echo /sbin/ifconfig`
+IPADDR=`$IFCONFIG eth0|gawk '/inet addr/{print $2}'|gawk -F: '{print $2}'`
+MSJ_NO_IP_DHCP="If you could not get a DHCP IP address please type setup and select \"Network configuration\" to set up a static IP."
+
+echo ""
+echo "Welcome to Elastix "
+echo "----------------------------------------------------"
+echo ""
+echo "For access to the Elastix web GUI use this URL"
+
+if [ "$IPADDR" = "" ]; then
+   echo "http://<YOUR-IP-HERE>"
+   echo "$MSJ_NO_IP_DHCP"
+else
+   echo "http://$IPADDR"
+fi
+
+echo ""
