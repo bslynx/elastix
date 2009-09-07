8304651e5767443e0fcf265187be9dc4adb99a14
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2009-09-07 20:12:07 +0000

    Branch 1.6

---
+++ additionals/branches/1.6/usr/local/sbin/motd.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+IFCONFIG=`which ifconfig 2>/dev/null||echo /sbin/ifconfig`
+IPADDR=`LANG=en_US.UTF-8 $IFCONFIG eth0|gawk '/inet addr/{print $2}'|gawk -F: '{print $2}'`
+MSJ_NO_IP_DHCP="If you could not get a DHCP IP address please type setup and select \"Network configuration\" to set up a static IP."
+
+echo ""
+echo "Welcome to Elastix "
+echo "----------------------------------------------------"
+echo ""
+#echo "For access to the Elastix web GUI use this URL"
+echo "To access your Elastix System, using a separate workstation (PC/MAC/Linux)"
+echo "Open the Internet Browser using the following URL:"
+
+if [ "$IPADDR" = "" ]; then
+   echo "http://<YOUR-IP-HERE>"
+   echo "$MSJ_NO_IP_DHCP"
+else
+   echo "http://$IPADDR"
+fi
+
+echo ""
