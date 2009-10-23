865dd041921305d0b518ee36e62cb576ee19b78c
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2009-10-23 20:58:47 +0000

    Base de datos hardware_detector.db, disponible

--- modules/trunk/core/system/setup/installer.php
+++ modules/trunk/core/system/setup/installer.php
@@ -30,9 +30,17 @@
 $DocumentRoot = (isset($_SERVER['argv'][1]))?$_SERVER['argv'][1]:"/var/www/html";
 $DataBaseRoot = "/var/www/db";
 $tmpDir = '/tmp/new_module/system';  # in this folder the load module extract the package content
+
 if(!file_exists("$DataBaseRoot/FTP.db")){
     $cmd_mv    = "mv $tmpDir/setup/FTP.db $DataBaseRoot/";
     $cmd_chown = "chown asterisk.asterisk $DataBaseRoot/FTP.db";
+    exec($cmd_mv);
+    exec($cmd_chown);
+}
+
+if(!file_exists("$DataBaseRoot/hardware_detector.db")){
+    $cmd_mv    = "mv $tmpDir/setup/hardware_detector.db $DataBaseRoot/";
+    $cmd_chown = "chown asterisk.asterisk $DataBaseRoot/hardware_detector.db";
     exec($cmd_mv);
     exec($cmd_chown);
 }
