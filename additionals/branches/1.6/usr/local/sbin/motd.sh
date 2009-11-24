125ce4d828e122c3642b3b66b6edda34cae4a986
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2009-11-24 22:33:15 +0000

    Work around an old bug in a wanpipe-util package by restoring a symlink that is incorrectly removed by the upgrade from said package. Required for successful upgrade to Elastix 1.6.

--- additionals/branches/1.6/usr/local/sbin/motd.sh
+++ additionals/branches/1.6/usr/local/sbin/motd.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+
+# Check for existence of /etc/init.d/wanrouter
+if [ ! -e /etc/init.d/wanrouter ] ; then
+        if [ -e /usr/sbin/wanrouter ] ; then
+                ln -s /usr/sbin/wanrouter /etc/init.d/wanrouter
+		service asterisk stop > /dev/null 2>&1
+		service dahdi stop > /dev/null 2>&1
+		service wanrouter stop > /dev/null 2>&1
+		service wanrouter start > /dev/null 2>&1
+		service dahdi start > /dev/null 2>&1
+		service asterisk start > /dev/null 2>&1
+        fi
+fi
+
 IFCONFIG=`which ifconfig 2>/dev/null||echo /sbin/ifconfig`
 IPADDR=`LANG=en_US.UTF-8 $IFCONFIG eth0|gawk '/inet addr/{print $2}'|gawk -F: '{print $2}'`
 MSJ_NO_IP_DHCP="If you could not get a DHCP IP address please type setup and select \"Network configuration\" to set up a static IP."
