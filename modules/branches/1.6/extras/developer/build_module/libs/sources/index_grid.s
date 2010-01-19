a4a2d8b38fc425eda2908bd89de48c25823f2795
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2010-01-19 16:56:45 +0000

    Cambio para arreglar el url en los reportes.

--- modules/branches/1.6/extras/developer/build_module/libs/sources/index_grid.s
+++ modules/branches/1.6/extras/developer/build_module/libs/sources/index_grid.s
@@ -111,7 +111,7 @@ function report{NAME_CLASS}($smarty, $module_name, $local_templates_dir, &$pDB, 
     }
     else{
         $oGrid->showFilter(trim($htmlFilter));
-        $content = "<form  method='POST' style='margin-bottom:0;' action=$url>".$oGrid->fetchGrid($arrGrid, $arrData,$arrLang)."</form>";
+        $content = "<form  method='POST' style='margin-bottom:0;' action=\"$url\">".$oGrid->fetchGrid($arrGrid, $arrData,$arrLang)."</form>";
     }
     //end grid parameters
 
