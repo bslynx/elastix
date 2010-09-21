a9419cc15fe576e309402f426c9d70d7e0d4432b
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2010-09-21 17:44:03 +0000

    Trunks Used per Hour: fix over-complicated query for active trunks from FreePBX database that resulted in misnamed trunk sources. Should fix Elastix bug #499.

--- modules/branches/1.6/extras/call_center/rep_trunks_used_per_hour/index.php
+++ modules/branches/1.6/extras/call_center/rep_trunks_used_per_hour/index.php
@@ -238,106 +238,16 @@ function createFieldFilter($arrLang, $arrTrunk){
     return $arrFormElements;
 }
 
-function getTrunk($pDB, $pDB_asterisk, $band=NULL){
-    $arrTrunk=array();
-    $oTrunk  = new paloTrunk($pDB_asterisk);
-    //$PBXTrunks = $oTrunk->getTroncales($pDB_asterisk);
-    $PBXTrunks = getTrunks($pDB_asterisk);
-    if (is_array($PBXTrunks)) {
-        foreach($PBXTrunks as $key => $value) {
-            $query = "SELECT variable, value from globals where variable='".$value[0]."'";
-            $result=$pDB_asterisk->getFirstRowQuery($query, true);
-            if (is_array($result) && count($result)>0) {
-		if(is_null($band))
-	                $arrTrunk[$result['value']] =  $result['value'];
-		else
-			$arrTrunk[$result['variable']] =  $result['value'];
-            }
-        }
+function obtener_nuevas_trunks($pDB, $pDB_asterisk)
+{
+    $listaTrunks = array();
+    $trunks = getTrunks($pDB_asterisk);    
+
+    foreach ($trunks as $tuplaTrunk) {
+        $listaTrunks[$tuplaTrunk[1]] = $tuplaTrunk[1];
     }
-    return $arrTrunk;
+    return $listaTrunks;
 }
-
-
-
-//diana nueva funcion que  extrae  las verdaderas trunks
-function obtener_nuevas_trunks($pDB, $pDB_asterisk){
-	//obtenemos el arreglo con indice variable valor
-	$arrTrunk = getTrunk($pDB, $pDB_asterisk, $band=1);
-
-	//obtenemos un arreglo de la tabla iax del asterisk con la forma indice para digito y valores de name y context
-	$arrIax = obtenerArregloIaxSip($pDB, $pDB_asterisk);
-
-	//asi mismo obtenemos un arreglo de la tabla sip del asterisk
-	$arrSip = obtenerArregloIaxSip($pDB, $pDB_asterisk, 'sip');
-
-	$arrNuevasTrunks = array();
-        $valor_iax = $valor_sip = "";
-
-	//primero recorremos el arreglo de trunks que existia
-	$contador = 0;
-	foreach($arrTrunk as $key=>$value){
-		$digito = substr($key, 4);//obtenemos solo el digito que acompania a OUT
-                //validaciones para valores  setaados 
-                $arrIax[$digito]['context_name'] = isset($arrIax[$digito]['context_name'])?$arrIax[$digito]['context_name']:'';
-                $arrIax[$digito]['trunk_name'] = isset($arrIax[$digito]['trunk_name'])?$arrIax[$digito]['trunk_name']:'';
-
-                $arrSip[$digito]['context_name'] = isset($arrSip[$digito]['context_name'])?$arrSip[$digito]['context_name']:'';
-                $arrSip[$digito]['trunk_name'] = isset($arrSip[$digito]['trunk_name'])?$arrSip[$digito]['trunk_name']:'';
-
-		//con este digito verificamos a que id de tabla iax corresponde, estos los puse en el arreglo $arrIax
-		$hasta = strpos($value, '/');//ubicacion del caracter / hasta
-		$cadena =  substr($value, 0 , $hasta);
-
-                /*validacion para formar los Zap, estos vienen con el nombre x defecto x eso solo hay que setearo directamente*/
-                if($cadena=='ZAP'){
-                    $arrNuevasTrunks[$value] = $value;//concatenamos la cadena completa del trunk ZAP
-                }
-
-		$valor_iax = ($arrIax[$digito]['context_name']!='')?$arrIax[$digito]['context_name']:$arrIax[$digito]['trunk_name'];
-		$valor_sip = ($arrSip[$digito]['context_name']!='')?$arrSip[$digito]['context_name']:$arrSip[$digito]['trunk_name'];
-
-		//validamos si es vacio alguno entonces no lo ponemos en el arreglo
-		if($valor_iax!='')
-			$arrNuevasTrunks[$cadena."/".$valor_iax] = $cadena."/".$valor_iax;//concatenamos la cadena completa del trunk IAX
-		elseif($valor_sip!='')
-			$arrNuevasTrunks[$cadena."/".$valor_sip] = $cadena."/".$valor_sip;//concatenamos la cadena completa del trunk SIP
-	}
-	return $arrNuevasTrunks;
-}
-
-//diana funcion para obtener arreglo de la tabla iax o sip del asterisk
-function obtenerArregloIaxSip($pDB, $pDB_asterisk, $valor=NULL){ 
-        $arrNuevasTrunks = array();
-	$arrIaxSip = array();
-	//Depende si es iax o sip armamos el query
-        $tabla = is_null($valor)?'iax':'sip';
-
-        //query a la tabla iax o sip
-	$query = "SELECT id, data from $tabla where id like '9999%' and keyword like 'account' and flags=2";
-        $result=$pDB_asterisk->FetchTable($query, true);
-        if (is_array($result) && count($result)>0) {
-		foreach($result as $key=>$value){
-			$arrIaxSip[$key]['id'] =  $value['id'];
-			$arrIaxSip[$key]['data'] = $value['data'];
-		}
-        }
-
-        foreach($arrIaxSip as $ind=>$data){
-                //si el indice tiene 4 nueves corresponde al name trunk, si tiene 5 nueves  es el context
-                $cant_nueves = substr_count($data['id'], 9, 0,5);//cantidad de 9 existentes
-                if($cant_nueves==4){
-                        $digito = substr($data['id'], 4);
-                        $arrNuevasTrunks[$digito]['trunk_name'] = $data['data'];
-                }elseif($cant_nueves==5){
-                        $digito = substr($data['id'], 5);
-                        $arrNuevasTrunks[$digito]['context_name'] = $data['data'];
-                }
-                $arrNuevasTrunks[$digito]['indice'] = $digito;
-        }
-    return $arrNuevasTrunks;
-}
-
 
 function getParameter($parameter)
 {
