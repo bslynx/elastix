92a5dca4680f168685926208276357a0cbc2e652
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2010-11-29 23:23:12 +0000

    FIXED: framework: rewrite of library for querying and removing Asterisk CDRs in MySQL. This rewrite achieves the following: * Simplify behavior of pattern search to a simple substring search, instead of trying to be smart with pattern string. Fixes Elastix bug #532. * Use SQL query parameters to remove opportunities for SQL injection. * Add inline documentation for the methods * Add alternate (hopefully simpler and easier to use) interface

--- additionals/branches/1.6/html/libs/paloSantoCDR.class.php
+++ additionals/branches/1.6/html/libs/paloSantoCDR.class.php
@@ -27,7 +27,8 @@
   +----------------------------------------------------------------------+
   $Id: paloSantoCDR.class.php,v 1.1.1.1 2007/07/06 21:31:55 gcarrillo Exp $ */
 
-class paloSantoCDR {
+class paloSantoCDR
+{
 
     function paloSantoCDR(&$pDB)
     {
@@ -48,95 +49,216 @@ class paloSantoCDR {
         }
     }
 
-    function obtenerCDRs($limit, $offset, $date_start="", $date_end="", $field_name="", $field_pattern="",$status="ALL",$calltype="",$troncales=NULL, $extension="")
+    private function _construirWhereCDR($param)
     {
-        $strWhere = "";
-        if(!empty($date_start)) $strWhere .= "calldate>='$date_start' ";
-        if(!empty($date_end))   $strWhere .= " AND calldate<='$date_end' ";
+        $condSQL = array();
+        $paramSQL = array();
 
-        if(!empty($field_name) and !empty($field_pattern)){
-                  $arrPattern = explode(",",trim($field_pattern));
-                  $condicion_pattern='';
-                 foreach ($arrPattern as $pattern) {
-                           $pattern = trim($pattern);
-                       if ($pattern!="") {
-                           if (ereg("/",$pattern)) {
-                                  if (!ereg("-",$pattern)) $pattern=$pattern.'-';
-                                  if ($field_name=='dst') continue;
-                           } else if ($field_name=='dstchannel') continue;
-                           $condicion_pattern.=!(empty($condicion_pattern))?' OR ':'';
-                           $condicion_pattern .= (ereg("/",$pattern) && $field_name=='src'?'channel':$field_name)." like '%$pattern%'";
-                       }
-                  }
-                  if ($condicion_pattern!="") $strWhere .= " AND ($condicion_pattern)";
+        // Fecha y hora de inicio y final del rango
+        $sRegFecha = '/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/';
+        if (isset($param['date_start']) && preg_match($sRegFecha, $param['date_start'])) {
+            $condSQL[] = 'calldate >= ?';
+            $paramSQL[] = $param['date_start'];
         }
-        if(!empty($status) && $status!="ALL") $strWhere .= " AND disposition = '$status' ";
-        if(!empty($calltype) && $calltype=="outgoing"){
-            if (is_array($troncales) && count($troncales)>0){
-                 $condicion_troncal='';
-                foreach ($troncales as $troncal){
-                   $condicion_troncal.=!(empty($condicion_troncal))?' OR ':'';
-                   $condicion_troncal.="dstchannel like '%$troncal-%'";
+        if (isset($param['date_end']) && preg_match($sRegFecha, $param['date_end'])) {
+            $condSQL[] = 'calldate <= ?';
+            $paramSQL[] = $param['date_end'];
+        }
+        
+        // Estado de la llamada
+        if (isset($param['status'])) {
+            $condSQL[] = 'disposition = ?';
+            $paramSQL[] = $param['status'];
+        }
+
+        // Extensión de fuente o destino
+        if (isset($param['extension'])) {
+            $condSQL[] = '(src = ? OR dst = ?)';
+            array_push($paramSQL, $param['extension'], $param['extension']);
+        }
+        
+        // Dirección de la llamada
+        if (isset($param['calltype']) && 
+            in_array($param['calltype'], array('incoming', 'outgoing'))) {
+            $sCampo = ($param['calltype'] == 'incoming') ? 'channel' : 'dstchannel';
+            $listaTroncales = array();
+            if (isset($param['troncales']) && is_array($param['troncales'])) {
+                $listaTroncales = $param['troncales'];
+            }
+
+            // TODO: expandir troncales al estilo DAHDI/g0
+
+            if (count($listaTroncales) > 0) {
+                /* Se asume que la lista de troncales es válida, y que todo canal
+                   empieza con la troncal correspondiente */
+                if (!function_exists('_construirWhereCDR_troncal2like')) {
+                    function _construirWhereCDR_troncal2like($s) { return $s.'%'; }
                 }
-                $strWhere .= " AND ($condicion_troncal)";
-            }else{
-                $strWhere .= " AND dstchannel like '%DAHDI%' ";
+                $paramSQL = array_merge($paramSQL, array_map('_construirWhereCDR_troncal2like', $listaTroncales));
+                $condSQL[] = '('.implode(' OR ', array_fill(0, count($listaTroncales), "$sCampo LIKE ?")).')';                
+            } else {
+                /* Filtrar por todo lo que parezca troncal. Actualmente se recoge 
+                   ZAP, DAHDI, y SIP/IAX2/H323 con un valor a la derecha que 
+                   contenga al menos un caracter alfabético.
+                   FIXME: no reconoce troncales enteramente numéricas que parecen teléfonos
+                   FIXME: no reconoce troncales si tienen caracteres no alfanuméricos
+                 */
+                $sRegExpTroncal = '^(ZAP/.+|DAHDI/.+|(SIP|IAX|IAX2|H323)/([[:alnum:]]*[[:alpha:]][[:alnum:]]*))-';
+                $condSQL[] = "$sCampo REGEXP '$sRegExpTroncal'";
             }
         }
-        if(!empty($calltype) && $calltype=="incoming") $strWhere .= " AND channel like '%DAHDI%' ";
-        if(!empty($extension)) $strWhere .= " AND (src='$extension' OR dst='$extension') ";
 
-        $query   = "SELECT calldate, src, dst, channel, dstchannel, disposition, uniqueid, duration, billsec, accountcode FROM cdr ";
-        // Clausula WHERE aqui
-        if(!empty($strWhere)) $query .= "WHERE $strWhere ";
-        // Limit
-        if(!empty($limit)) {
-            $query  .= " LIMIT $limit OFFSET $offset";
+        // field_name, field_pattern
+        if (isset($param['field_name']) && isset($param['field_pattern'])) {            
+            /* No se intenta interpretar field_pattern. Únicamente se construye
+               la condición LIKE para que el campo correspondiente contenga como
+               subcadena el valor de field_pattern. */
+            $sCampo = $param['field_name'];
+            if (!function_exists('_construirWhereCDR_novacio')) {
+                function _construirWhereCDR_novacio($s) { return ($s != ''); }
+            }
+            $listaPat = array_filter(
+                array_map('trim', 
+                    is_array($param['field_pattern']) 
+                        ? $param['field_pattern'] 
+                        : explode(',', trim($param['field_pattern']))), 
+                '_construirWhereCDR_novacio');
+
+            if (!function_exists('_construirWhereCDR_troncal2like2')) {
+                function _construirWhereCDR_troncal2like2($s) { return '%'.$s.'%'; }
+            }
+            $paramSQL = array_merge($paramSQL, array_map('_construirWhereCDR_troncal2like2', $listaPat));
+            $condSQL[] = '('.implode(' OR ', array_fill(0, count($listaPat), "$sCampo LIKE ?")).')';                
         }
 
-        $result=$this->_DB->fetchTable($query);
-        $arrResult['Data'] = $result;
-
-        $queryCount = "SELECT COUNT(*) FROM cdr ";
-        // Clausula WHERE aqui
-        if(!empty($strWhere)) $queryCount .= " WHERE $strWhere ";
-
-        $arrResult['NumRecords'] = $this->_DB->getFirstRowQuery($queryCount);
-
-        return $arrResult;
+        // Construir fragmento completo de sentencia SQL
+        $where = array(implode(' AND ', $condSQL), $paramSQL);
+        if ($where[0] != '') $where[0] = 'WHERE '.$where[0];
+        return $where;
     }
 
+    /**
+     * Procedimiento para listar los CDRs desde la tabla asterisk.cdr con varios
+     * filtrados aplicados.
+     *
+     * @param   mixed   $param  Lista de parámetros de filtrado:
+     *  date_start      Fecha y hora minima de la llamada, en formato 
+     *                  yyyy-mm-dd hh:mm:ss. Si se omite, se lista desde la 
+     *                  primera llamada.
+     *  date_end        Fecha y hora máxima de la llamada, en formato 
+     *                  yyyy-mm-dd hh:mm:ss. Si se omite, se lista hasta la 
+     *                  última llamada.
+     *  status          Estado de la llamada, guardado en el campo 'disposition'.
+     *                  Si se especifica, puede ser uno de los valores siguientes:
+     *                  ANSWERED, NO ANSWER, BUSY, FAILED
+     *  calltype        Tipo de llamada. Se puede indicar "incoming" o "outgoing".
+     *  troncales       Arreglo de troncales por el cual se debe filtrar las
+     *                  llamadas según el valor almacenado en la columna 'channel'
+     *                  o 'dstchannel', para calltype de tipo "incoming" o 
+     *                  "outgoing", respectivamente. Se ignora si se omite un
+     *                  valor para calltype.
+     *  extension       Número de extensión para el cual filtrar los números. 
+     *                  Este valor filtra por los campos 'src' y 'dst'.
+     *  field_name
+     *  field_pattern   Campo y subcadena para buscar dentro de los registros.
+     *                  El valor de field_pattern puede ser un arreglo, o un
+     *                  valor separado por comas, y buscará múltiples patrones.
+     * @param   mixed   $limit  Máximo número de CDRs a leer, o NULL para todos
+     * @param   mixed   $offset Inicio de lista de CDRs, si se especifica $limit
+     *
+     * @return  mixed   Estructura con los siguientes campos:
+     *  total   integer     Número total de CDRs disponibles con los filtrados
+     *  cdrs    mixed       Lista de los cdrs. Se devuelven los siguientes campos
+     *                      en el orden en que se listan a continuación:
+     *                      calldate, src, dst, channel, dstchannel, disposition, 
+     *                      uniqueid, duration, billsec, accountcode
+     */
+    function listarCDRs($param, $limit = NULL, $offset = 0)
+    {
+        $resultado = array();
+        list($sWhere, $paramSQL) = $this->_construirWhereCDR($param);
+
+        // Cuenta del total de registros recuperados
+        $sPeticionSQL = "SELECT COUNT(*) FROM cdr $sWhere";
+        $r = $this->_DB->getFirstRowQuery($sPeticionSQL, FALSE, $paramSQL);
+        if (!is_array($r)) {
+            $this->errMsg = '(internal) Failed to count CDRs - '.$this->_DB->errMsg;
+            return NULL;
+        }
+        $resultado['total'] = $r[0];
+        $resultado['cdrs'] = array();
+        if ($resultado['total'] <= 0) return $resultado;
+
+        // Los datos de los registros, respetando limit y offset
+        $sPeticionSQL = 
+            "SELECT calldate, src, dst, channel, dstchannel, disposition, uniqueid, duration, billsec, accountcode ".
+            "FROM cdr $sWhere ORDER BY calldate";
+        if (!empty($limit)) {
+            $sPeticionSQL .= " LIMIT ? OFFSET ?";
+            array_push($paramSQL, $limit, $offset);
+        }
+        $resultado['cdrs'] = $this->_DB->fetchTable($sPeticionSQL, FALSE, $paramSQL);
+        if (!is_array($resultado['cdrs'])) {
+            $this->errMsg = '(internal) Failed to fetch CDRs - '.$this->_DB->errMsg;
+            return NULL;
+        }
+        return $resultado;
+    }
+    
+    /**
+     * Procedimiento para borrar los CDRs en la tabla asterisk.cdr que coincidan
+     * con los filtros indicados.
+     * @param   mixed   $param  Lista de parámetros de filtrado. Véase listarCDRs
+     *                          para los parámetros permitidos.
+     *
+     * @return  bool    VERDADERO en caso de éxito, FALSO en caso de error.
+     */
+    function borrarCDRs($param)
+    {
+        list($sWhere, $paramSQL) = $this->_construirWhereCDR($param);
+
+        // Borrado de los registros seleccionados
+        $sPeticionSQL = "DELETE FROM cdr $sWhere";
+        $r = $this->_DB->genQuery($sPeticionSQL);
+        if (!$r) {
+            $this->errMsg = '(internal) Failed to delete CDRs - '.$this->_DB->errMsg;
+        }
+        return $r;
+    }
+    
+    // Función de compatibilidad para código antiguo
+    function obtenerCDRs($limit, $offset, $date_start="", $date_end="", $field_name="", $field_pattern="",$status="ALL",$calltype="",$troncales=NULL, $extension="")
+    {
+        $param = array();
+        if (!empty($date_start)) $param['date_start'] = $date_start;
+        if (!empty($date_end)) $param['date_end'] = $date_end;
+        if (!empty($field_name)) $param['field_name'] = $field_name;
+        if (!empty($field_pattern)) $param['field_pattern'] = $field_pattern;
+        if (!empty($status) && $status != 'ALL') $param['status'] = $status;
+        if (!empty($calltype)) $param['calltype'] = $calltype;
+        if (!empty($troncales)) $param['troncales'] = $troncales;
+        if (!empty($extension)) $param['extension'] = $extension;
+        $r = $this->listarCDRs($param, $limit, $offset);
+        return is_array($r) 
+            ? array(
+                'NumRecords'    =>  array($r['total']),
+                'Data'          =>  $r['cdrs'],
+                )
+            : NULL;
+    }
+
+    // Función de compatibilidad para código antiguo
     function Delete_All_CDRs($date_start="", $date_end="", $field_name="", $field_pattern="",$status="ALL",$calltype="",$troncales=NULL)
     {
-        $strWhere = "";
-        if(!empty($date_start)) $strWhere .= "calldate>='$date_start' ";
-        if(!empty($date_end))   $strWhere .= " AND calldate<='$date_end' ";
-
-        if(!empty($field_name) and !empty($field_pattern)) $strWhere .= " AND $field_name like '%$field_pattern%' ";
-        if(!empty($status) && $status!="ALL") $strWhere .= " AND disposition = '$status' ";
-        if(!empty($calltype) && $calltype=="outgoing"){
-            if (is_array($troncales) && count($troncales)>0){
-                 $condicion_troncal='';
-                foreach ($troncales as $troncal){
-                   $condicion_troncal.=!(empty($condicion_troncal))?' OR ':'';
-                   $condicion_troncal.="dstchannel like '%$troncal%'";
-                }
-                $strWhere .= " AND ($condicion_troncal)";
-            }else{
-                $strWhere .= " AND dstchannel like '%DAHDI%' ";
-            }
-        }
-        if(!empty($calltype) && $calltype=="incoming") $strWhere .= " AND channel like '%DAHDI%' ";
-
-
-        $query = "DELETE FROM cdr ";
-
-        if(!empty($strWhere)) $query .= "WHERE $strWhere ";
-
-        $result = $this->_DB->genQuery($query);
-        if($result[0] > 0)
-            return true;
-        else return false;
+        $param = array();
+        if (!empty($date_start)) $param['date_start'] = $date_start;
+        if (!empty($date_end)) $param['date_end'] = $date_end;
+        if (!empty($field_name)) $param['field_name'] = $field_name;
+        if (!empty($field_pattern)) $param['field_pattern'] = $field_pattern;
+        if (!empty($status) && $status != 'ALL') $param['status'] = $status;
+        if (!empty($calltype)) $param['calltype'] = $calltype;
+        if (!empty($troncales)) $param['troncales'] = $troncales;
+        return $this->borrarCDRs($param);
     }
 }
 ?>
