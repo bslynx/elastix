fd08af63849ecd210369c7659b34dd36ecf30caa
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2010-12-08 16:16:54 +0000

    CHANGED: Module faxnew, Fixed Hard to see Bug  (H2C Bug), on paloSantoFax.class.php _deleteLinesFromInittab  MUST be called using $devId instead $idFax. Code Improvement, class paloSantoFax.class.php, a new function called  restartFax() was created. www.bugs.elastix.org [#607].

--- modules/trunk/core/fax/modules/faxnew/index.php
+++ modules/trunk/core/fax/modules/faxnew/index.php
@@ -136,6 +136,7 @@ function _moduleContent(&$smarty, $module_name)
             // TODO: Debo revisar por errores aqui
             $oFax->createFaxExtension($_POST['name'], $_POST['extension'], $_POST['secret'], $_POST['email'], 
                                       $_POST['clid_name'], $_POST['clid_number'],$_POST['country_code'],$_POST['area_code']);
+            $oFax->restartFax();
             header("Location: ?menu=faxlist");
         } else {
             // Error
@@ -156,7 +157,11 @@ function _moduleContent(&$smarty, $module_name)
         if($oForm->validateForm($_POST)) {
             // Exito, puedo procesar los datos ahora.
             $oFax = new paloFax();
-            $oFax->editFaxExtension($_POST['id_fax'],$_POST['name'], $_POST['extension'],                              $_POST['secret'], $_POST['email'],$_POST['clid_name'],                             $_POST['clid_number'],$_POST['dev_id'], $_POST['port'],$_POST['country_code'],$_POST['area_code']);
+            $oFax->editFaxExtension($_POST['id_fax'],$_POST['name'], $_POST['extension'],
+                              $_POST['secret'], $_POST['email'],$_POST['clid_name'],
+                              $_POST['clid_number'],$_POST['dev_id'],
+                              $_POST['port'],$_POST['country_code'],$_POST['area_code']);
+            $oFax->restartFax();
             header("Location: ?menu=faxlist");
         } else {
             // Error
@@ -176,6 +181,7 @@ function _moduleContent(&$smarty, $module_name)
         //- TODO: Validar el id de fax
         $oFax = new paloFax(); 
         $oFax->deleteFaxExtensionById($_POST['id_fax']);
+        $oFax->restartFax();
         header("Location: ?menu=faxlist");
 
     } else if(isset($_POST['edit'])) {
