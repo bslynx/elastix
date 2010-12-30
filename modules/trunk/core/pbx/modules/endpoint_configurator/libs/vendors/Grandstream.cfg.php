d9f47536f585dd39fe79e505c5a5fbceafacfc28
asantos87 <asantos87@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2010-12-30 22:14:12 +0000

    CHANGED: module endpoint_configuration, new parameter for the phone GXV3140

--- modules/trunk/core/pbx/modules/endpoint_configurator/libs/vendors/Grandstream.cfg.php
+++ modules/trunk/core/pbx/modules/endpoint_configurator/libs/vendors/Grandstream.cfg.php
@@ -3,43 +3,83 @@
     PrincipalFileGrandstream nos retorna el contenido del archivo de configuracion de los EndPoint
     Grandstream, para ello es necesario enviarle el DisplayName, id_device, secret, ipAdressServer
 */
-function PrincipalFileGrandstream($DisplayName, $id_device, $secret, $ipAdressServer)
+function PrincipalFileGrandstream($DisplayName, $id_device, $secret, $ipAdressServer, $model)
 {
-    $content="
-
-# Firmware Server Path
-P192 = $ipAdressServer
-
-# Config Server Path
-P237 = $ipAdressServer
-
-# Firmware Upgrade. 0 - TFTP Upgrade,  1 - HTTP Upgrade.
-P212 = 0
-
-# Account Name
-P270 = $DisplayName
-
-# SIP Server
-P47 = $ipAdressServer
-
-# Outbound Proxy
-P48 = $ipAdressServer
-
-# SIP User ID
-P35 = $id_device
-
-# Authenticate ID
-P36 = $id_device
-
-# Authenticate password
-P34 = $secret
-
-# Display Name (John Doe)
-P3 = $DisplayName
-
-# DHCP support. 0 - yes, 1 - no
-P8 = 1";
-
+    if($model == "GXV3140"){
+            $content="
+        # Firmware Server Path
+        P192 = $ipAdressServer
+        
+        # Config Server Path
+        P237 = $ipAdressServer
+        
+        # Firmware Upgrade. 0 - TFTP Upgrade,  1 - HTTP Upgrade.
+        P212 = 0
+        
+        # Account Name
+        #P270 = $DisplayName
+        P417 = $DisplayName
+        
+        # SIP Server
+        #P47 = $ipAdressServer
+        P402 = $ipAdressServer
+        
+        # Outbound Proxy
+        #P48 = $ipAdressServer
+        P403 = $ipAdressServer
+        
+        # SIP User ID
+        #P35 = $id_device
+        P404 = $id_device
+        
+        # Authenticate ID
+        #P36 = $id_device
+        P405 = $id_device
+        
+        # Authenticate password
+        #P34 = $secret
+        P406 = $secret
+        
+        # Display Name (John Doe)
+        #P3 = $DisplayName
+        P407 = $DisplayName";
+    }
+    else{
+        $content="
+    
+    # Firmware Server Path
+    P192 = $ipAdressServer
+    
+    # Config Server Path
+    P237 = $ipAdressServer
+    
+    # Firmware Upgrade. 0 - TFTP Upgrade,  1 - HTTP Upgrade.
+    P212 = 0
+    
+    # Account Name
+    P270 = $DisplayName
+    
+    # SIP Server
+    P47 = $ipAdressServer
+    
+    # Outbound Proxy
+    P48 = $ipAdressServer
+    
+    # SIP User ID
+    P35 = $id_device
+    
+    # Authenticate ID
+    P36 = $id_device
+    
+    # Authenticate password
+    P34 = $secret
+    
+    # Display Name (John Doe)
+    P3 = $DisplayName
+    
+    # DHCP support. 0 - yes, 1 - no
+    P8 = 1";
+    }
     return $content;
 }
 
