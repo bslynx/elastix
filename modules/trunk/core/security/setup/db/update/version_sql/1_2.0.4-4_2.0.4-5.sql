b64b98826cdc245270759b1e4c05cfde2cd5f7ec
asantos87 <asantos87@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2011-02-02 16:51:10 +0000

    CHANGED: only the new column was added

--- modules/trunk/core/security/setup/db/update/version_sql/1_2.0.4-4_2.0.4-5.sql
+++ modules/trunk/core/security/setup/db/update/version_sql/1_2.0.4-4_2.0.4-5.sql
@@ -1,7 +1,2 @@
 ALTER TABLE filter ADD COLUMN state varchar(50);
 
-UPDATE filter set order=20 where traffic='INPUT' and eth_in='ANY' and eth_out='' and ip_source='0.0.0.0/0' and ip_destiny='0.0.0.0/0' and protocol='ALL' and sport='' and dport='' and icmp_type='' and number_ip='' and target='REJECT';
-UPDATE filter set order=21 where traffic='FORWARD' and eth_in='ANY' and eth_out='ANY' and ip_source='0.0.0.0/0' and ip_destiny='0.0.0.0/0' and protocol='ALL' and sport='' and dport='' and icmp_type='' and number_ip='' and target='REJECT';
-
-INSERT INTO filter(traffic,eth_in,eth_out,ip_source,ip_destiny,protocol,sport,dport,icmp_type,number_ip,target,rule_order,activated,state) VALUES ('INPUT','ANY','','0.0.0.0/0','0.0.0.0/0','STATE','','','','','ACCEPT',19,1,'Established,Related');
-
