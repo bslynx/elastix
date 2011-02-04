50afbad3675e1bb6babca3fbeeca7e5801d65e15
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2011-02-04 15:33:48 +0000

    CHANGED: Agenda Modules, Ahora la base de datos tipo sqlite3 que eran archivos binarios, se cambian a archivos SQLs nativos para que la base de datos sea creada por elastix-dbprocess.

---
+++ modules/trunk/core/agenda/setup/db/install/address_book/1_schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE contact (
+     id          integer primary key autoincrement,
+     name        varchar(35),
+     last_name   varchar(35),
+     telefono    varchar(12),
+     extension   varchar(7),
+     email       varchar(30),
+     iduser      int,
+     picture     varchar(50),
+     address     varchar(100),
+     company     varchar(30),
+     notes       varchar(200),
+     status      varchar(30)
+);
