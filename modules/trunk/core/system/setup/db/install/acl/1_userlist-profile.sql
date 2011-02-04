e5d096a4f3ba4aaec3971c6e15552a25d77fd062
edu19432 <edu19432@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2011-02-04 23:16:51 +0000

    CHANGED:   Put lines transaccion and commit in script db/install/acl/1_userlist-profile.sql

--- modules/trunk/core/system/setup/db/install/acl/1_userlist-profile.sql
+++ modules/trunk/core/system/setup/db/install/acl/1_userlist-profile.sql
@@ -1,20 +1,22 @@
+BEGIN TRANSACTION;
 CREATE TABLE acl_profile_properties
 (
-    id_profile   INTEGER NOT NULL,
-    property varchar(32) NOT NULL,
-    value varchar(256),
-
-    PRIMARY KEY (id_profile, property),
-    FOREIGN KEY (id_profile) REFERENCES acl_user_profile (id_profile)
+       id_profile   INTEGER NOT NULL,
+       property varchar(32) NOT NULL,
+       value varchar(256),
+       
+       PRIMARY KEY (id_profile, property),
+       FOREIGN KEY (id_profile) REFERENCES acl_user_profile (id_profile)
 );
 CREATE TABLE acl_user_profile
 (
-    id_profile   INTEGER NOT NULL,
-    id_user INTEGER NOT NULL,
-    id_resource  INTEGER NOT NULL,
-    profile      VARCHAR(32) NOT NULL,
+       id_profile   INTEGER NOT NULL,
+       id_user INTEGER NOT NULL,
+       id_resource  INTEGER NOT NULL,
+       profile      VARCHAR(32) NOT NULL,
 
-    PRIMARY KEY (id_profile),
-    FOREIGN KEY (id_user) REFERENCES acl_user(id),
-    FOREIGN KEY (id_resource) REFERENCES acl_resource(id)
+       PRIMARY KEY (id_profile),
+       FOREIGN KEY (id_user) REFERENCES acl_user(id),
+       FOREIGN KEY (id_resource) REFERENCES acl_resource(id)
 );
+COMMIT;
