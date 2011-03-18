ce145603b38653ba03f06fb7b1b159970db94017
edu19432 <edu19432@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2011-03-18 00:57:19 +0000

    NEW:  email_admin - antispam:  New script in php to delete message from Spam folder per each email account

---
+++ modules/trunk/core/email_admin/setup/deleteSpam.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+// script para crear carpetas spam y suscribirlas
+    $module_name = "antispam";
+    include_once "/var/www/html/libs/misc.lib.php";
+    include_once "/var/www/html/libs/paloSantoDB.class.php";
+    include_once "/var/www/html/configs/email.conf.php";
+    include_once "/var/www/html/libs/cyradm.php";
+    include_once "/var/www/html/modules/$module_name/libs/paloSantoAntispam.class.php";
+
+    $pDB = new paloDB("sqlite3:////var/www/db/email.db");
+    $objAntispam = new paloSantoAntispam("", "", "", "");
+    // primero se verifica si alguna cuenta no tiene la carpeta Spam creada
+    $accounts = $objAntispam->getEmailList($pDB);
+
+    exec("/etc/init.d/spamassassin status", $flag, $status);
+
+    if($status == 0){
+        // si el arreglo es vacio no hace nada
+        if(isset($accounts) & $accounts!=""){// existe alguna cuenta sin esa carpeta
+            foreach($accounts as $key => $value){
+                $status .= $objAntispam->deleteSpamMessages($value['username']);
+            }
+        }
+    }else{
+        echo "ERROR: ".$flag[0]."\n";
+    }
+
+?>
