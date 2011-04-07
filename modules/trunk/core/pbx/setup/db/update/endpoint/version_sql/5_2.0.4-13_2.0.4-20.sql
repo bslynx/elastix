2b1501c0627c5053f7257a411e0ae0e21dc4d8c7
asantos87 <asantos87@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2011-04-07 17:42:21 +0000

    ADDED: module endpoint_configurator, added a new sql file for phones Yealink models T20, T22, T26 and T28

---
+++ modules/trunk/core/pbx/setup/db/update/endpoint/version_sql/5_2.0.4-13_2.0.4-20.sql
@@ -0,0 +1,11 @@
+INSERT INTO vendor (name,description) VALUES('Yealink','Yealink');
+
+INSERT INTO mac (id_vendor,value,description) VALUES(13,'00:15:65','Yealink');
+
+INSERT INTO model (name,description,id_vendor) VALUES('T20','T20',13);
+
+INSERT INTO model (name,description,id_vendor) VALUES('T22','T22',13);
+
+INSERT INTO model (name,description,id_vendor) VALUES('T26','T26',13);
+
+INSERT INTO model (name,description,id_vendor) VALUES('T28','T28',13);
