6e7eb6286d785598107a53992830b48862a14bbe
bmacias <bmacias@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2011-05-31 19:33:34 +0000

    CHANGED: Module Time Config, se cambio de lugar al módulo time config, paso de framework a modules/core/system.

---
+++ modules/trunk/core/system/modules/time_config/themes/default/js/javascript.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    $( "#datepicker" ).datepicker({
+            changeMonth: true,
+            changeYear: true,
+            dateFormat:'dd M yy'
+     });
+    //document.getElementById("datepicker").value = document.getElementById("date_hiden").value;
+});
