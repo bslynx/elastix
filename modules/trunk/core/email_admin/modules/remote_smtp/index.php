ea717d9f3d94b1eb5941e65c69271cc3638de2e0
asantos87 <asantos87@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2011-10-26 22:49:09 +0000

    CHANGED: module remote_smtp, the module title is now handled by the framework

--- modules/trunk/core/email_admin/modules/remote_smtp/index.php
+++ modules/trunk/core/email_admin/modules/remote_smtp/index.php
@@ -102,7 +102,7 @@ function viewFormEmailRelay($smarty, $module_name, $local_templates_dir, &$pDB, 
     $smarty->assign("STATUS",$arrLang['Status']);
     $smarty->assign("MSG_REMOTE_SMTP",$arrLang['Message Remote SMTP Server']);
     $smarty->assign("MSG_REMOTE_AUT",$arrLang['Message Remote Autentification']);
-    $smarty->assign("IMG", "images/list.png");
+    $smarty->assign("icon", "images/list.png");
     $smarty->assign("Example",_tr("Ex"));
     $smarty->assign("lbldomain",$arrLang["Domain"]);
 
