ae55135b4688e3e1e6042c97f585494eab359c33
asantos87 <asantos87@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2011-10-27 21:42:47 +0000

    FIXED: module festival, messages was not displaying for theme elastixneo

--- modules/trunk/core/pbx/modules/festival/themes/default/form.tpl
+++ modules/trunk/core/pbx/modules/festival/themes/default/form.tpl
@@ -1,8 +1,3 @@
-<table width="100%" border="0" cellspacing="0" cellpadding="4" align="center">
-    <tr class="moduleTitle">
-        <td class="moduleTitle" valign="middle" colspan='2'>&nbsp;&nbsp;<img src="{$IMG}" border="0" align="absmiddle">&nbsp;&nbsp;{$title}</td>
-    </tr>
-</table>
 <table class="tabForm" style="font-size: 16px;" width="100%" >
     <tr class="letra12">
         <td align="left" width="10%"><b>{$status.LABEL}: </b></td>
--- modules/trunk/core/pbx/modules/festival/themes/default/js/javascript.js
+++ modules/trunk/core/pbx/modules/festival/themes/default/js/javascript.js
@@ -20,10 +20,26 @@ $(document).ready(function (){
 			{   
 			    if(arrData["mb_title"] && arrData["mb_message"]){
 				$("#message_error").remove();
-				var message= "<div style='background-color: rgb(255, 238, 255);' id='message_error'><table width='100%'><tr><td align='left'><b style='color:red;'>" +
-					      arrData['mb_title'] + "</b>" + arrData['mb_message'] + "</td> <td align='right'><input type='button' onclick='hide_message_error();' value='" +
-					      arrData['button_title']+ "'/></td></tr></table></div>";
-				$("body > table > tbody > tr > td:last").prepend(message);
+				if($(".neo-module-content")){
+				    var message= "<div class='div_msg_errors' id='message_error'>" +
+						      "<div style='float:left;'>" +
+							  "<b style='color:red;'>&nbsp;&nbsp;"+arrData['mb_title']+"</b>" +
+						      "</div>" +
+						      "<div style='text-align:right; padding:5px'>" +
+							  "<input type='button' onclick='hide_message_error();' value='"+arrData['button_title']+"'/>" +
+						      "</div>" +
+						      "<div style='position:relative; top:-12px; padding: 0px 5px'>" +
+							  arrData['mb_message'] +
+						      "</div>" +
+						  "</div>";
+				    $(".neo-module-content:first").prepend(message);
+				}
+				else{
+				    var message= "<div style='background-color: rgb(255, 238, 255);' id='message_error'><table width='100%'><tr><td align='left'><b style='color:red;'>" +
+						  arrData['mb_title'] + "</b>" + arrData['mb_message'] + "</td> <td align='right'><input type='button' onclick='hide_message_error();' value='" +
+						  arrData['button_title']+ "'/></td></tr></table></div>";
+				    $("body > table > tbody > tr > td:last").prepend(message);
+				}
 			    }
 			}
 		    );
