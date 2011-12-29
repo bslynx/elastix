b2b42dee6fb5481b032a14998ed06fad21da3338
asantos87 <asantos87@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2011-12-29 15:51:27 +0000

    ADDED: modules addons_availables, added the embedded help for this module

--- modules/trunk/core/addons/modules/addons_availables/index.php
+++ modules/trunk/core/addons/modules/addons_availables/index.php
@@ -241,7 +241,8 @@ function do_listarAddons($smarty, $module_name, $local_templates_dir)
 	'INSTALL'	=>  _tr('INSTALL'),
 	'UPDATE'	=>  _tr('UPDATE'),
 	'UNINSTALL'	=>  _tr('UNINSTALL'),
-	'more_info'	=>  _tr('More info')
+	'more_info'	=>  _tr('More info'),
+	'location'	=>  _tr('Location')
     ));
     $respuesta['addonlist_html'] = $smarty->fetch("$local_templates_dir/reporte_addons_lista.tpl");
     return $json->encode($respuesta);
