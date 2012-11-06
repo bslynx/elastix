b5d39045420ae729e66f52054165b47ad1e47bbc
a_villacis <a_villacis@9cfa96a4-f038-0410-bb5f-a5d7b009fd84>
2012-11-06 18:09:28 +0000

    CHANGED: Send Fax: partial cleanup:  Do not silently ignore failure to submit a fax job, and display error instead.  Remove useless code that could potentially error out the module.  Remove needless copy of temporary file followed by manual delete. Use the temporary uploaded file directly.  Remove file type validation based on file extension. It is easy to beat, also prevents legitimate text files from being uploaded, and sendfax already has to figure out file type in order to apply conversion.

--- branches/2.3.0/apps/core/fax/modules/sendfax/libs/paloSantoSendFax.class.php
+++ branches/2.3.0/apps/core/fax/modules/sendfax/libs/paloSantoSendFax.class.php
@@ -56,7 +56,10 @@ class paloSantoSendFax {
         $destine = escapeshellarg($destine);
         $data = escapeshellarg($data);
         $output = $retval = NULL;
-        exec("sendfax -D -h $faxhost -n -d $destine $data", $output, $retval);
+        exec("sendfax -D -h $faxhost -n -d $destine $data 2>&1", $output, $retval);
+        if ($retval != 0) {
+            $this->errMsg = implode('<br/>', $output);
+        }
         return ($retval == 0);
     }
 }
